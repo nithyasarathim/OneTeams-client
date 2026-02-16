141c6c1fcafbc7ac401c963be5a7241d840904b4
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2026-02-16 10:41:18 +0530

    refactor : [callback] all the credentials are taken from .env

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,2 +1,6 @@
 export const TIMEOUT = import.meta.env.VITE_REQUEST_TIME_OUT;
-export const APP_SERVER_URL = import.meta.env.VITE_APP_SERVER_API_URL;
+export const APP_SERVER_URL = import.meta.env.VITE_SERVER_API_URL;
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+export const REDIRECT_URL = import.meta.env.VITE_CLIENT_REDIRECT_URL;
+export const AUTH_CLIENT_URL = import.meta.env.VITE_AUTH_CLIENT_URL;
+export const CLIENT_STATE = import.meta.env.VITE_CLIENT_STATE;
